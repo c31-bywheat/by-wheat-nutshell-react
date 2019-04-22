47b02fd35230f881117222200df9d54f6427bd71
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-22 15:21:46 -0500

    Switching branches

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import Nutshell from './components/Nutshell'
 import './index.css'
-import MessageManager from "./modules/MessageManager.js"
 
-const testMessage = {
-  "message": "hola"
-}
-
-MessageManager.postMessage(testMessage)
 
 
 ReactDOM.render(
