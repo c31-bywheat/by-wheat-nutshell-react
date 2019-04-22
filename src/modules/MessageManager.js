44a96c0c1abc29121fa4fbe4f562199ac513e858
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-22 13:37:24 -0500

    Created fetch calls for messages

---
+++ src/modules/MessageManager.js
@@ -0,0 +1,28 @@
+import { getEnabledCategories } from "trace_events";
+
+const apiURL = "http://localhost:5002"
+
+export default {
+    getAll() {
+        return fetch(`${apiURL}/messages`)
+        .then(messages => messages.json())
+    },
+    getOne(id) {
+        return fetch(`${apiURL}/messages/${id}`)
+    },
+    deleteMessage(id) {
+        return fetch(`${apiURL}/messages/${id}`, {
+            method: "DELETE"
+        })
+        .then(message => message.json())
+    },
+    postMessage(newMessage) {
+        return fetch(`${apiURL}/messages`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newMessage)
+        }).then(message => message.json())
+    }
+}
