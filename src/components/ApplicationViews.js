6e41d1b0bb12dbd65d4efbb7de3004b3699f6f2a
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-23 15:46:46 -0500

    Got authentication working for all pages using session storage

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,3 @@
-import { Route } from "react-router-dom";
 import React, { Component } from "react";
 import NewMessage from "./messages/NewMessage"
 import NewsList from "./news/NewsList";
@@ -8,11 +7,14 @@ import EventManager from "../modules/EventManager";
 import MessageManager from "../modules/MessageManager"
 import NewsForm from "../components/news/NewsForm"
 import MessageEditForm from "./messages/MessageEditForm"
+import Login from "./Authentication/Login"
+import { Route, Redirect } from "react-router-dom"
 
 
 
 export default class ApplicationViews extends Component {
 
+  isAuthenticated = () => sessionStorage.getItem("credentials") !== null
 
   state = {
     "users": [],
@@ -96,63 +98,93 @@ export default class ApplicationViews extends Component {
   render() {
     return (
       <React.Fragment>
+        
 
         <Route
           exact path="/login" render={props => {
-            return null
-            // Remove null and return the component which will handle authentication
+            return <Login users={this.state.users} {...props}/>
+           
           }}
         />
-
+        
         <Route
           exact path="/" render={props => {
+            if(this.isAuthenticated()) {
             return <NewsList {...props}
             deleteNews={this.deleteNews}
             addNews={this.addNews}
             articles={this.state.articles} />
-            // Remove null and return the component which will show news articles
-          }}
+           
+          } else {
+            return <Redirect to="/login"/>
+          }
+        
+        }}
         />
 
         <Route
           exact path="/articles/new" render={props => {
+            if(this.isAuthenticated()) {
             return <NewsForm {...props} articles={this.state.articles}
             addNews={this.addNews} />
+          } else {
+            return <Redirect to="/login"/>
           }
-        }
+        }}
         />
         <Route
           path="/friends" render={props => {
+            if(this.isAuthenticated()) {
             return null
             // Remove null and return the component which will show list of friends
-          }}
+          }else {
+            return <Redirect to="/login"/>
+          }
+        }
+        }
         />
 
         <Route
           exact path="/messages" render={props => {
+            if(this.isAuthenticated()) {
             return <NewMessage {...props} messages={this.state.messages} deleteMessage={this.deleteMessage}
             postMessage={this.postMessage} makeNewMessage={this.makeNewMessage}/>
             // Remove null and return the component which will show the messages
-          }}
+          }else {
+            return <Redirect to="/login"/>
+          }
+        }}
         />
 
         <Route
           path="/events" render={props => {
+            if(this.isAuthenticated()) {
             return null
             // Remove null and return the component which will show the user's events
-          }}
+          }else {
+            return <Redirect to="/login"/>
+          }
+        }}
         />
 
         <Route
           path="/tasks" render={props => {
+            if(this.isAuthenticated()) {
             return null
             // Remove null and return the component which will show the user's tasks
-          }}
+          } else {
+            return <Redirect to="/login"/>
+          }
+        }}
         />
         <Route
           path="/messages/:messageId(\d+)/edit" render={props => {
+            if(this.isAuthenticated()) {
             return <MessageEditForm {...props} messages={this.state.messages} editMessage={this.editMessage}/>
-            }}
+            } else {
+              return <Redirect to="/login"/>
+            }
+          }}
           />
 
       </React.Fragment>
