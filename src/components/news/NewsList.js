d14e46890f152490cb96bb0371874f53d9457271
Josh Hibray <jjhibray@gmail.com>
2019-04-25 12:02:48 -0500

    commit before merge

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -19,15 +19,18 @@ export default class NewsList extends Component {
             {
                 this.props.articles.map(article =>
                     <div key={article.id} className="card">
-                        <div className="card-body"> {article.title}
-                        {article.synopsis} {article.url}{article.timestamp}
-                            <h5 className="card-title">
+                        <div className="card-body">
+                        <h5 className="card-title">
+                        <h3>{article.title}</h3>
+                        <p>{article.synopsis}</p>
+                        <p><a href={`http://${article.url}`} target="_blank">{article.url}</a></p>
+                        <p className="timestamp">{article.timestamp}</p>
                                 <button
                                     onClick={() => this.props.deleteNews(article.id)}
-                                    className="card-link">Delete</button>
+                                    className="card-delete">Delete</button>
                                 <button
                                     onClick={() => this.props.history.push(`/articles/${article.id}/edit`)}
-                                    className="editButton">Edit</button>
+                                    className="card-edit">Edit</button>
                             </h5>
                         </div>
                     </div>
